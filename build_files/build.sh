2392c16058c3efa8484e876a357fe4039d76eebd
Oliver Gregory <oliverjamesgregory99@gmail.com>
2026-01-08 14:45:04 +0000

    add dependencies for vterm in emacs

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y --skip-unavailable emacs neovim coolercontrol liquidctl openrazer-daemon
+dnf5 install -y --skip-unavailable emacs neovim coolercontrol liquidctl openrazer-daemon cmake libvterm libtool
 
 # Use a COPR Example:
 #
