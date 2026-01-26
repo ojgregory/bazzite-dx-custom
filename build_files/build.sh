c31aa8ef391b57e941289ef32cbda5418f582f85
Oliver Gregory <oliverjamesgregory99@gmail.com>
2026-01-26 12:36:30 +0000

    Add -y to dnf

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,9 +12,9 @@ set -ouex pipefail
 # this installs a package from fedora repos
 dnf5 install -y --skip-unavailable emacs neovim coolercontrol liquidctl openrazer-daemon cmake libvterm libtool mupdf mupdf-devel mupdf-libs emacs-jinx
 
-dnf5 copr enable avengemedia/dms
-dnf5 install niri dms
-systemctl --global add-wants niri.service dms
+dnf5 copr -y enable avengemedia/dms
+dnf5 install -y niri dms
+systemctl -y --global add-wants niri.service dms
 
 dnf5 -y copr disable avengemedia/dms
 # Use a COPR Example:
